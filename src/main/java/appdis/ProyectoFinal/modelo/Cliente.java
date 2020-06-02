d9d2313eca27d255bf506a2017d044b9b019b9a2
Geovanny21094 <gioabad210@hotmail.com>
2020-06-02 11:50:44 -0500

    agregando modelos

--- src/main/java/appdis/ProyectoFinal/modelo/Cliente.java
+++ src/main/java/appdis/ProyectoFinal/modelo/Cliente.java
@@ -1,10 +1,52 @@
 package appdis.ProyectoFinal.modelo;
 
+import java.sql.Date;
+
 public class Cliente {
 	
 	
 	private int id_cliente;
 	private Date fecha_registro;
+	private String usuario;
+	private String contrasenia;
+	
+	public int getId_cliente() {
+		return id_cliente;
+	}
+	
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+	
+	public Date getFecha_registro() {
+		return fecha_registro;
+	}
+	
+	public void setFecha_registro(Date fecha_registro) {
+		this.fecha_registro = fecha_registro;
+	}
+	
+	public String getUsuario() {
+		return usuario;
+	}
+	
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
