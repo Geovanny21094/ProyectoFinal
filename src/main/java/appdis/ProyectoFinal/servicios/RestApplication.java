b65a8fd39a94ce996a540a9f8cc8f42083831f02
Diego Rodriguez A <diego.gibi.97@gmail.com>
2020-06-30 22:14:32 -0500

    RESTCajero

--- src/main/java/appdis/ProyectoFinal/servicios/RestApplication.java
+++ src/main/java/appdis/ProyectoFinal/servicios/RestApplication.java
@@ -4,6 +4,10 @@ package appdis.ProyectoFinal.servicios;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(description = "Servidor local", url = "/ProyectoFinal") })
 @ApplicationPath("/rs")
 public class RestApplication extends Application{
 
